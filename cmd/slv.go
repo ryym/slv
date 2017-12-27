a2e511699b03f4f988e9ad0d533ebbafce47e5aa
ryym <ryym.64@gmail.com>
2017-12-27 17:10:26 +0900

    feat(compile): Enable to compile without running

--- cmd/slv.go
+++ cmd/slv.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/ryym/slv/slv"
 	"github.com/ryym/slv/slv/t"
@@ -24,6 +26,12 @@ func CreateApp() *cli.App {
 			Usage:     "Run tests for the specified source code",
 			ArgsUsage: "[src]",
 			Action:    cmdTest,
+		},
+		{
+			Name:      "compile",
+			Usage:     "Compile without running",
+			ArgsUsage: "[src]",
+			Action:    cmdCompile,
 		},
 	}
 	return app
@@ -57,5 +65,33 @@ func cmdTest(c *cli.Context) error {
 	if !ok {
 		os.Exit(1)
 	}
+	return nil
+}
+
+func cmdCompile(c *cli.Context) error {
+	if c.NArg() != 1 {
+		cli.ShowCommandHelpAndExit(c, "compile", 0)
+	}
+
+	conf, err := slv.MakeExecConf(c.Args()[0])
+	if err != nil {
+		return cli.NewExitError(err.Error(), 1)
+	}
+
+	execPath, err := slv.Compile(&conf)
+	if err != nil {
+		return cli.NewExitError(err.Error(), 1)
+	}
+
+	// Try to use relative path.
+	pwd, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err == nil {
+		relPath, err := filepath.Rel(pwd, execPath)
+		if err == nil && len(relPath) < len(execPath) {
+			execPath = relPath
+		}
+	}
+
+	fmt.Printf("Compiled to %s\n", execPath)
 	return nil
 }
---
+++ slv/compile.go
@@ -0,0 +1,30 @@
+package slv
+
+import (
+	"errors"
+
+	"github.com/ryym/slv/slv/prgs"
+	"github.com/ryym/slv/slv/probdir"
+	"github.com/ryym/slv/slv/t"
+)
+
+func Compile(c *t.ExecConf) (string, error) {
+	prg, err := prgs.FindProgram(c.SrcPath)
+	if err != nil {
+		return "", err
+	}
+
+	destDir := probdir.GetDestDir(c)
+	cmds := prg.GetCompileCmds(c.SrcPath, destDir)
+
+	if cmds.Cmds == nil {
+		return "", errors.New("This does not need compilation")
+	} else {
+		err = compileProgram(&cmds, destDir)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return cmds.ExecPath, nil
+}
