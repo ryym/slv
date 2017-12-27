236ad9d4121052174c4fb9b2e18dc9c3a79f693c
ryym <ryym.64@gmail.com>
2017-12-27 17:04:53 +0900

    refac(test): Separate responsibilities more properly

--- slv/test.go
+++ slv/test.go
@@ -13,7 +13,7 @@ import (
 )
 
 func TestAll(c *t.ExecConf) (bool, error) {
-	execCmds, err := compileProgram(c)
+	execCmds, err := findAndCompile(c)
 	if err != nil {
 		return false, err
 	}
@@ -23,7 +23,7 @@ func TestAll(c *t.ExecConf) (bool, error) {
 	return test.TestAll(execCmds, testdir, printer)
 }
 
-func compileProgram(c *t.ExecConf) ([]string, error) {
+func findAndCompile(c *t.ExecConf) ([]string, error) {
 	prg, err := prgs.FindProgram(c.SrcPath)
 	if err != nil {
 		return nil, err
@@ -31,28 +31,30 @@ func compileProgram(c *t.ExecConf) ([]string, error) {
 
 	destDir := fmt.Sprintf("%s/%s.built", c.WorkDir, c.SrcFile)
 	cmds := prg.GetCompileCmds(c.SrcPath, destDir)
-	err = compileIfNeed(&cmds, destDir)
-	if err != nil {
-		return nil, err
+
+	if cmds.Cmds != nil {
+		err = compileProgram(&cmds, destDir)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return prg.GetExecCmds(cmds.ExecPath), nil
 }
 
-func compileIfNeed(cmds *t.CompileCmds, destDir string) error {
-	if cmds.Cmds != nil {
-		_, err := os.Stat(destDir)
-		if os.IsNotExist(err) {
-			err = os.Mkdir(destDir, 0755)
-		}
-		if err != nil {
-			return errors.Wrap(err, "Failed to create work dir")
-		}
-
-		out, err := exec.Command(cmds.Cmds[0], cmds.Cmds[1:]...).CombinedOutput()
-		if err != nil {
-			return errors.Wrap(err, string(out))
-		}
+func compileProgram(cmds *t.CompileCmds, destDir string) error {
+	_, err := os.Stat(destDir)
+	if os.IsNotExist(err) {
+		err = os.Mkdir(destDir, 0755)
 	}
+	if err != nil {
+		return errors.Wrap(err, "Failed to create work dir")
+	}
+
+	out, err := exec.Command(cmds.Cmds[0], cmds.Cmds[1:]...).CombinedOutput()
+	if err != nil {
+		return errors.Wrap(err, string(out))
+	}
+
 	return nil
 }
