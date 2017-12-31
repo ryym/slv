9cc2c19a8ea84abee7f0604e383632c77cf8cf01
ryym <ryym.64@gmail.com>
2017-12-31 17:13:01 +0900

    feat: Support more languages

--- slv/config.go
+++ slv/config.go
@@ -15,4 +15,24 @@ run = "ruby {{.Src}}"
 exts = [".scala"]
 compile = "scalac -d {{.Dest}} {{.Src}}"
 run = "scala -cp {{.Dest}} Main"
+
+[lang.java]
+exts = [".java"]
+compile = "javac -d {{.Dest}} {{.Src}}"
+run = "java -cp {{.Dest}} Main"
+
+[lang.rust]
+exts = [".rs"]
+compile = "rustc -o {{.Dest}}/out {{.Src}}"
+run = "{{.Dest}}/out"
+
+# XXX: What is a most common C++ compiler?
+[lang."c++"]
+exts = [".cpp"]
+compile = "c++ -o {{.Dest}}/out {{.Src}}"
+run = "{{.Dest}}/out"
+
+[lang.javascript]
+exts = [".js"]
+run = "node {{.Src}}"
 `
